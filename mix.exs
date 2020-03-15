c0e59b354f894ea521e5d243ecda9981773a4424
Nathaniel Barnes <nathaniel.r.barnes@gmail.com>
2020-03-15 10:24:45 -0700

    Added doc generation

--- mix.exs
+++ mix.exs
@@ -23,6 +23,7 @@ defmodule AliceRoller.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
+      {:ex_doc, ">= 0.0.0", only: :dev, runtime: false},
       {:alice, "~> 0.3.7"},
       {:high_roller, "~> 0.3.2"}
     ]
