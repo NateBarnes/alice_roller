59c84464d7edd6aebfd07057682577ebd4dc3d97
Nathaniel Barnes <nathaniel.r.barnes@gmail.com>
2020-03-18 20:17:34 -0700

    Adjust regex for roll

--- lib/alice/handlers/roller.ex
+++ lib/alice/handlers/roller.ex
@@ -2,7 +2,7 @@ defmodule Alice.Handlers.Roller do
   use Alice.Router
 
   command ~r/roll (?<term>.+)/i, :fetch
-  route   ~r/roll (?<term>.+)/i, :fetch
+  route   ~r/^(alice )?roll (?<term>.+)/i, :fetch
 
   @doc "`roll 3d6+1` - rolls 3 d6s and adds 1"
   def fetch(conn) do
