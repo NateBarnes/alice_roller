2c619fa4cdd4e63c4f256ebdda5bb41dea7628b1
Nathaniel Barnes <nathaniel.r.barnes@gmail.com>
2020-04-10 17:45:47 -0700

    Update for new verion of alice

--- test/alice/handlers/roller_test.exs
+++ test/alice/handlers/roller_test.exs
@@ -1,24 +1,21 @@
 defmodule Alice.Handlers.RollerTest do
   use ExUnit.Case
-  import Alice.Handlers.Case
+  use Alice.Handlers.Case, handlers: Alice.Handlers.Roller
 
   test "it rolls a single dice appropriately" do
-    fake_conn_with_capture("roll 3d1+1", ~r/roll (?<term>.+)/i)
-    |> Alice.Handlers.Roller.roll
+    receive_message("roll 3d1+1")
 
     assert first_reply() == "4\n[1, 1, 1] + 1"
   end
 
   test "it rolls multiple dice appropriately" do
-    fake_conn_with_capture("roll 3d1+1+3d1", ~r/roll (?<term>.+)/i)
-    |> Alice.Handlers.Roller.roll
+    receive_message("roll 3d1+1+3d1")
 
     assert first_reply() == "7\n[1, 1, 1] + 1 + [1, 1, 1]"
   end
 
   test "it fails gracefully" do
-    fake_conn_with_capture("roll invalid", ~r/roll (?<term>.+)/i)
-    |> Alice.Handlers.Roller.roll
+    receive_message("roll invalid")
 
     assert first_reply() == "Curiouser and curiouser! I cannot make heads nor tails of that!"
   end
