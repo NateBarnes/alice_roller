57040566763829e15617d094039d7f18e1cc13fd
Nathaniel Barnes <nathaniel.r.barnes@gmail.com>
2020-04-26 09:34:19 -0700

    Change alice branch

--- mix.exs
+++ mix.exs
@@ -26,7 +26,7 @@ defmodule AliceRoller.MixProject do
   defp deps do
     [
       {:ex_doc, ">= 0.0.0", only: :dev, runtime: false},
-      {:alice, "~> 0.4.3", override: true},
+      {:alice, "~> 0.4.3", github: "alice-bot/alice", override: true},
       {:high_roller, "~> 0.4.1"},
       {:excoveralls, "~> 0.12.3", only: :test}
     ]
